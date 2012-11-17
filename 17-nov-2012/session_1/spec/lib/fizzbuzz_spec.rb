2fc94ac17b8c0f942dc41f51d0676450f9725c2f
halan <halan.pinheiro@gmail.com>
2012-11-17 14:23:03 -0300

    Improve specs

--- 17-nov-2012/session_1/spec/lib/fizzbuzz_spec.rb
+++ 17-nov-2012/session_1/spec/lib/fizzbuzz_spec.rb
@@ -2,37 +2,43 @@ require 'spec_helper'
 
 describe FizzBuzz do
   subject { FizzBuzz.new length }
-  let(:length) { 100 }
 
-  its(:to_ary) { should have(length).items }
+  context 'with length 100' do
+    let(:length) { 100 }
 
-  describe 'Fizz' do
-    it 'returned by [3]' do
-      subject[3].should == 'Fizz'
+    its(:to_ary) { should have(length).items }
+    its(:first) { should == 1 }
+
+    describe 'Fizz' do
+      it 'returned by [3]' do
+        subject[3].should == 'Fizz'
+      end
+
+      it 'not returned by [2]' do
+        subject[2].should_not == 'Fizz'
+      end
     end
 
-    it 'not returned by [2]' do
-      subject[2].should_not == 'Fizz'
+    describe 'Buzz' do
+      it 'returned by [5]' do
+        subject[5].should == 'Buzz'
+      end
+
+      its(:last) { should == 'Buzz' }
+
+      it 'not returned by [2]' do
+        subject[2].should_not == 'Buzz'
+      end
+    end
+
+    describe 'FizzBuzz' do
+      it 'returned by [15]' do
+        subject[15].should == 'FizzBuzz'
+      end
+
+      it 'not returned by [2]' do
+        subject[2].should_not == 'Buzz'
+      end
     end
   end
-
-  describe 'Buzz' do
-    it 'returned by [5]' do
-      subject[5].should == 'Buzz'
-    end
-
-    it 'not returned by [2]' do
-      subject[2].should_not == 'Buzz'
-    end
   end
-
-  describe 'FizzBuzz' do
-    it 'returned by [15]' do
-      subject[15].should == 'FizzBuzz'
-    end
-
-    it 'not returned by [2]' do
-      subject[2].should_not == 'Buzz'
-    end
-  end
-end
