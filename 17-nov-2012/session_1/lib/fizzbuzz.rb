190d5f18b967243c25b2bd034a0f2c57fe7dfa7d
halan <halan.pinheiro@gmail.com>
2012-11-17 14:19:27 -0300

    Fizzbuzz!

--- 17-nov-2012/session_1/lib/fizzbuzz.rb
+++ 17-nov-2012/session_1/lib/fizzbuzz.rb
@@ -1,5 +1,22 @@
 class FizzBuzz < Array
   def initialize length
-    super length
+    super (1..length).to_a
+    process!
+  end
+
+  def [] number
+    super number-1
+  end
+
+  protected
+  def process!
+    map! {|n| calc n }
+  end
+
+  def calc number
+    if    (number % 3).zero?  and (number % 5).zero? then 'FizzBuzz'
+    elsif (number % 3).zero? then 'Fizz'
+    elsif (number % 5).zero? then 'Buzz'
+    else  number end
   end
 end
--- 17-nov-2012/session_1/spec/lib/fizzbuzz_spec.rb
+++ 17-nov-2012/session_1/spec/lib/fizzbuzz_spec.rb
@@ -5,4 +5,34 @@ describe FizzBuzz do
   let(:length) { 100 }
 
   its(:to_ary) { should have(length).items }
+
+  describe 'Fizz' do
+    it 'returned by [3]' do
+      subject[3].should == 'Fizz'
+    end
+
+    it 'not returned by [2]' do
+      subject[2].should_not == 'Fizz'
+    end
+  end
+
+  describe 'Buzz' do
+    it 'returned by [5]' do
+      subject[5].should == 'Buzz'
+    end
+
+    it 'not returned by [2]' do
+      subject[2].should_not == 'Buzz'
+    end
+  end
+
+  describe 'FizzBuzz' do
+    it 'returned by [15]' do
+      subject[15].should == 'FizzBuzz'
+    end
+
+    it 'not returned by [2]' do
+      subject[2].should_not == 'Buzz'
+    end
+  end
 end
